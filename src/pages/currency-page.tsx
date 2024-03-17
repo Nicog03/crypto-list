287c0db8c7aa9d9410c85cedf7bd00c5362a9e1d
nicolas <nicolas.lima03@hotmail.com>
2024-03-17 20:06:25 -0300

    implement a call to the api to retrieve the specific coin information

--- src/pages/currency-page.tsx
+++ src/pages/currency-page.tsx
@@ -1,3 +1,16 @@
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { getSpecificCoinData } from '../../api/requests';
+
 export default function CurrencyPage() {
+  const { currencyId } = useParams();
+
+  const { data } = useQuery({
+    queryKey: [`${currencyId}-data`],
+    queryFn: () => getSpecificCoinData(currencyId!),
+  });
+
+  console.log(data);
+
   return <h1>Currency Page</h1>;
 }
