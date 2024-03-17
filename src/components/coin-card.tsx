3adddb4d437e46aff4e0015c714512b8d46ca50f
nicolas <nicolas.lima03@hotmail.com>
2024-03-16 23:13:31 -0300

    change card style & redirect to currency page on click

--- src/components/coin-card.tsx
+++ src/components/coin-card.tsx
@@ -1,3 +1,5 @@
+import { useNavigate } from 'react-router-dom';
+
 interface CoinCardInterface {
   coinName: string;
   coinImageUrl: string;
@@ -9,12 +11,23 @@ export default function CoinCard({
   coinImageUrl,
   coinPrice,
 }: CoinCardInterface) {
+  const navigate = useNavigate();
+
+  function changePageHandler() {
+    const currencyName = coinName.toLocaleLowerCase().replace(/ /g, '-');
+
+    navigate(`${currencyName}`);
+  }
+
   return (
-    <button className="w-36 h-36 flex flex-col items-center gap-1 bg-gray-100 p-3 rounded-3xl hover:brightness-110">
+    <button
+      onClick={changePageHandler}
+      className="w-full h-fit flex items-center gap-4 bg-white/0 p-4 rounded-3xl hover:brightness-110 transition shadow-lg ring-1 ring-black/0 backdrop-blur-md"
+    >
       <img src={coinImageUrl} alt="bitcoin svg" className="h-14 w-14" />
-      <div className="flex flex-col">
+      <div className="flex flex-col items-start">
         <p className="font-semibold text-gray-700 text-lg">{coinName}</p>
-        <p className="text font-semibold text-gray-800">{coinPrice}</p>
+        <p className="text font-semibold text-gray-800">USD {coinPrice}</p>
       </div>
     </button>
   );
