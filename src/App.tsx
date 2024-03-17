9ca069a587a5b32bac13a4a7d86448cf32981951
nicolas <nicolas.lima03@hotmail.com>
2024-03-16 23:12:04 -0300

    implement initial routes logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import HomePage from './pages/homepage';
+import CurrencyPage from './pages/currency-page';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: ':currencyName',
+    element: <CurrencyPage />,
+  },
+]);
+
 function App() {
-  return <h1>Crypto List</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
