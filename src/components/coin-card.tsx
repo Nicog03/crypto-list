b7a286013e739a6e9fd132b77cef63186d23c66b
nicolas <nicolas.lima03@hotmail.com>
2024-03-17 13:38:37 -0300

    slightly change card style

--- src/components/coin-card.tsx
+++ src/components/coin-card.tsx
@@ -22,12 +22,12 @@ export default function CoinCard({
   return (
     <button
       onClick={changePageHandler}
-      className="w-full h-fit flex items-center gap-4 bg-white/0 p-4 rounded-3xl hover:brightness-110 transition shadow-lg ring-1 ring-black/0 backdrop-blur-md"
+      className="w-full h-fit flex items-center gap-4 bg-white/0 p-4 rounded-3xl hover:brightness-110 transition ring-1 ring-black/0 backdrop-blur-md border-[1px] border-white/5 shadow-inner shadow-gray-500/5"
     >
       <img src={coinImageUrl} alt="bitcoin svg" className="h-14 w-14" />
       <div className="flex flex-col items-start">
-        <p className="font-semibold text-gray-700 text-lg">{coinName}</p>
-        <p className="text font-semibold text-gray-800">USD {coinPrice}</p>
+        <p className="font-semibold text-gray-300 text-lg">{coinName}</p>
+        <p className="text font-semibold text-gray-200">USD {coinPrice}</p>
       </div>
     </button>
   );
