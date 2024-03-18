90230ee2458d99b4022b75b7360104ccb45529f8
nicolas <nicolas.lima03@hotmail.com>
2024-03-18 15:40:25 -0300

    define currency type

---
+++ src/shared/types/currency.type.ts
@@ -0,0 +1,20 @@
+export type CurrencyInfo = {
+  id: string;
+  symbol: string;
+  name: string;
+  market_data: MarketDataType;
+};
+
+export type MarketDataType = {
+  market_cap: { usd: number };
+  fully_diluted_valuation: { usd: number };
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  price_change_percentage_24h: number;
+  price_change_percentage_7d: number;
+  price_change_percentage_14d: number;
+  price_change_percentage_30d: number;
+  price_change_percentage_60d: number;
+  price_change_percentage_1y: number;
+};
