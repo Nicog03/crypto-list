b497da31aa97a79afd915e45c6c8c39343c79bec
nicolas <nicolas.lima03@hotmail.com>
2024-03-19 13:05:08 -0300

    component creation: footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="bg-black px-4 py-6">
+      <p className="text-white  ">
+        © 2024,{' '}
+        <a className="hover:underline" href="https://github.com/Nicog03">
+          Nicog03
+        </a>
+      </p>
+    </footer>
+  );
+}
