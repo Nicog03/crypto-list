fd6462d293a23805b69be812a83dcc5f234c72ea
nicolas <nicolas.lima03@hotmail.com>
2024-03-19 13:12:36 -0300

    feature: display price variation (%) in the last 24h

--- src/components/molecules/price-section.tsx
+++ src/components/molecules/price-section.tsx
@@ -1,17 +1,36 @@
 import { USDollar } from '../../utils/currencyUtils';
+import GreenCaret from '../../assets/icons/up-green-caret.svg';
+import RedCaret from '../../assets/icons/down-red-caret.svg';
 
 interface PriceSectionInterface {
   symbol: string;
   price: number;
+  dayValue: number;
 }
 
-export default function PriceSection({ symbol, price }: PriceSectionInterface) {
+export default function PriceSection({
+  symbol,
+  price,
+  dayValue,
+}: PriceSectionInterface) {
   return (
     <section>
       <h2 className="font-semibold text-xl text-gray-500">
         {symbol.toUpperCase()} Price
       </h2>
-      <p className="text-5xl font-bold">{USDollar.format(price)}</p>
+      <div className="flex items-center gap-2">
+        <p className="text-5xl font-bold">{USDollar.format(price)}</p>
+        <span className="flex items-center gap-1">
+          <img src={+dayValue > 0 ? GreenCaret : RedCaret} alt="" />
+          <p
+            className={`text-xl font-semibold ${
+              dayValue >= 0 ? `text-green-700` : `text-red-500`
+            }`}
+          >
+            {dayValue.toFixed(1).replace('-', '')}%
+          </p>
+        </span>
+      </div>
     </section>
   );
 }
