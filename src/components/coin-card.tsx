5c671462e0071606c1fa69eb4a72a53f486f481f
nicolas <nicolas.lima03@hotmail.com>
2024-03-19 00:26:52 -0300

    include compact chart component & modify card style

--- src/components/coin-card.tsx
+++ src/components/coin-card.tsx
@@ -1,10 +1,12 @@
 import { useNavigate } from 'react-router-dom';
+import CompactChart from './molecules/compact-chart';
 
 interface CoinCardInterface {
   coinName: string;
   coinImageUrl: string;
   coinPrice: number;
   coinId: string;
+  sparkline_in_7d: { price: number[] };
 }
 
 export default function CoinCard({
@@ -12,6 +14,7 @@ export default function CoinCard({
   coinImageUrl,
   coinPrice,
   coinId,
+  sparkline_in_7d,
 }: CoinCardInterface) {
   const navigate = useNavigate();
 
@@ -24,13 +27,24 @@ export default function CoinCard({
   return (
     <button
       onClick={changePageHandler}
-      className="w-full h-fit flex items-center gap-4 bg-white/0 p-4 rounded-3xl hover:brightness-110 transition ring-1 ring-black/0 backdrop-blur-md border-[1px] border-white/5 shadow-inner shadow-gray-500/5"
+      className="w-full h-fit flex justify-between max-w-[500px] items-center bg-white/0 p-4 rounded-3xl hover:brightness-110 transition ring-1 ring-black/0 backdrop-blur-sm hover:backdrop-blur-md border-[1px] border-white/5 shadow-inner shadow-gray-500/5"
     >
-      <img src={coinImageUrl} alt="bitcoin svg" className="h-14 w-14" />
-      <div className="flex flex-col items-start">
-        <p className="font-semibold text-gray-300 text-lg">{coinName}</p>
-        <p className="text font-semibold text-gray-200">USD {coinPrice}</p>
+      <div className="flex gap-4">
+        <img
+          src={coinImageUrl}
+          alt="bitcoin svg"
+          className="h-14 w-14 self-center"
+        />
+        <div className="flex flex-col items-start">
+          <p className="font-semibold text-gray-300 text-lg text-left">
+            {coinName}
+          </p>
+          <p className="text font-semibold text-gray-200 text-left">
+            USD {coinPrice}
+          </p>
+        </div>
       </div>
+      <CompactChart chartData={sparkline_in_7d.price} />
     </button>
   );
 }
