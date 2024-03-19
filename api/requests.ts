08fcdb277e7d97466a096cace45f09d5b3d62537
nicolas <nicolas.lima03@hotmail.com>
2024-03-18 21:58:37 -0300

    new fetch request: for the currency history data

--- api/requests.ts
+++ api/requests.ts
@@ -19,3 +19,20 @@ export async function getSpecificCoinData(coinId: string) {
   const coin = await res.json();
   return coin;
 }
+
+export async function getCoinHistoricalChartData({
+  coinId,
+  days,
+}: {
+  coinId: string;
+  days: number;
+}) {
+  const res = await fetch(
+    `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=usd&days=${days}&x_cg_demo_api_key=${
+      import.meta.env.VITE_API_KEY
+    }`
+  );
+
+  const chartData = await res.json();
+  return chartData;
+}
