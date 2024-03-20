0771b2e50dc7f21d533719069c309daad9a8f109
nicolas <nicolas.lima03@hotmail.com>
2024-03-20 00:15:43 -0300

    include metamask component on header & small change on style

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
 import BlockChainIcon from '../assets/icons/blockchain-icon.svg';
 import { useParams } from 'react-router-dom';
+import MetamaskSection from './metamask-section';
 
 export default function Header({ invert = false }: { invert: boolean }) {
   const { currencyId } = useParams();
@@ -8,12 +9,15 @@ export default function Header({ invert = false }: { invert: boolean }) {
 
   return (
     <header
-      className={`flex justify-center p-2 bg-black/5 ${invert ? 'invert' : ''}`}
+      className={`relative flex justify-center items-center p-2 bg-black/5 ${
+        invert ? 'bg-white/5' : ''
+      }`}
     >
-      <div className="flex items-center gap-3">
+      <div className={`flex items-center gap-3 ${invert ? 'invert' : ''}`}>
         <img src={BlockChainIcon} alt="blockchain icon" />
         <h1 className="font-semibold text-4xl">Crypto List</h1>
       </div>
+      <MetamaskSection invert={currencyId ? true : false} />
     </header>
   );
 }
