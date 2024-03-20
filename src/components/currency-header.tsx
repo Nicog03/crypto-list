0a4435f1c23a73ea329bf965c6d3336d7035a870
nicolas <nicolas.lima03@hotmail.com>
2024-03-20 14:16:16 -0300

    feature: create return button

--- src/components/currency-header.tsx
+++ src/components/currency-header.tsx
@@ -1,3 +1,6 @@
+import { Link } from 'react-router-dom';
+import leftCaret from '../assets/icons/left-caret.svg';
+
 interface CurrencyHeaderInterface {
   name: string;
   image: string;
@@ -11,6 +14,17 @@ export default function CurrencyHeader({
 }: CurrencyHeaderInterface) {
   return (
     <div className="flex items-center gap-4">
+      <Link
+        to={'/'}
+        className=" bg-gray-300 rounded-lg p-1 hover:brightness-75 transition"
+      >
+        <img
+          src={leftCaret}
+          alt="left facing caret"
+          className="h-8 w-8 opacity-50"
+        />
+      </Link>
+
       <img src={image} alt="currency image" />
       <h1 className="text-3xl font-semibold">{name}</h1>
       <p className="text-sm text-gray-700 bg-gray-200 font-bold p-1 rounded-lg px-2  text-center">
