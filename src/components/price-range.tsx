014de449b2db25bd3c6eacc8a52c5575b97ff948
nicolas <nicolas.lima03@hotmail.com>
2024-03-20 11:39:00 -0300

    input style change

--- src/components/price-range.tsx
+++ src/components/price-range.tsx
@@ -27,7 +27,25 @@ export default function PriceRange({
         </div>
       </div>
       <input
-        className="w-full "
+        className="relative appearance-none w-full bg-transparent
+        [&::-webkit-slider-thumb]:appearance-none
+        [&::-webkit-slider-thumb]:h-5
+        [&::-webkit-slider-thumb]:w-2
+        [&::-webkit-slider-thumb]:-mt-2
+        [&::-webkit-slider-thumb]:rounded-full
+        [&::-webkit-slider-thumb]:bg-gray-400
+        [&::-webkit-slider-thumb]:bottom-2
+        [&::-moz-range-thumb]:h-5
+        [&::-moz-range-thumb]:w-2
+        [&::-moz-range-thumb]:rounded-full
+        [&::-moz-range-thumb]:bg-gray-400
+        [&::-moz-range-thumb]:border-none
+        [&::-webkit-slider-runnable-track]:h-1                  
+        [&::-webkit-slider-runnable-track]:bg-gray-200                  
+        [&::-webkit-slider-runnable-track]:rounded-full                  
+        [&::-moz-range-track]:bg-gray-200                  
+        [&::-moz-range-track]:rounded-full
+"
         type="range"
         id="price-range"
         min={low}
