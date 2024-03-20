8d2791a02eab4fc83db98c6cdc9af53baefeaba5
nicolas <nicolas.lima03@hotmail.com>
2024-03-20 18:39:53 -0300

    remove inver prop from header & set some responsive related tags on wrapping div

--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -4,9 +4,9 @@ import Header from '../components/header';
 
 export default function Root() {
   return (
-    <div className="flex flex-col min-h-screen max-h-fit">
-      <Header invert={true} />
-      <div className="flex-grow">
+    <div className="flex flex-col min-h-screen max-h-fit sm:text-lg xl:max-w-[1426px] 2xl:max-w-[1526px] mx-auto">
+      <Header />
+      <div className="flex-grow flex">
         <Outlet />
       </div>
       <Footer />
