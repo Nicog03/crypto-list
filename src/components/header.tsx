7b4aed5b0cc64cdb086d7b0039efd34995842e62
nicolas <nicolas.lima03@hotmail.com>
2024-03-21 11:27:31 -0300

    make app header responsive

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,12 @@
 import BlockChainIcon from '../assets/icons/blockchain-icon.svg';
 import MetamaskSection from './metamask-section';
+import { useMediaQuery } from 'react-responsive';
 
 export default function Header() {
+  const isScreenSm = useMediaQuery({
+    query: '(min-width: 640px)',
+  });
+
   return (
     <header
       className={`relative flex justify-between items-center p-2 bg-black/5 dark:bg-white/5 z-10`}
@@ -10,9 +15,15 @@ export default function Header() {
         <img
           src={BlockChainIcon}
           alt="blockchain icon"
-          className="dark:invert"
+          className={`${isScreenSm ? 'h-14' : 'h-8'} dark:invert`}
         />
-        <h1 className="font-semibold text-4xl dark:text-white">Crypto List</h1>
+        <h1
+          className={`${
+            isScreenSm ? 'text-4xl' : 'text-2xl'
+          } font-semibold  dark:text-white`}
+        >
+          Crypto List
+        </h1>
       </div>
       <MetamaskSection />
     </header>
